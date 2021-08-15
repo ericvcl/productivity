8751d0bb051c3c903ff1dd0c9a462e9780e15813
ericvcl <eric.latham.2@gmail.com>
2021-08-15 13:13:29 -0700

    complete basic functionality

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import { useState, useEffect } from "react";
+import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import TaskCol from './TaskCol';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    loadTasksFromLocalStorage();
+  }, []);
+
+  function addEmptyTask(status) {
+    const lastTask = tasks[tasks.length - 1];
+
+    let newTaskId = 1;
+
+    if (lastTask !== undefined) {
+      newTaskId = lastTask.id + 1;
+    }
+
+    setTasks((tasks) => [
+      ...tasks,
+      {
+        id: newTaskId,
+        title: "",
+        description: "",
+        urgency: "",
+        status: status,
+      },
+    ]);
+  }
+
+  function addTask(taskToAdd) {
+    let filteredTasks = tasks.filter((task) => {
+      return task.id !== taskToAdd.id;
+    });
+
+    let newTaskList = [...filteredTasks, taskToAdd];
+
+    setTasks(newTaskList);
+
+    saveTasksToLocalStorage(newTaskList);
+  }
+
+  function deleteTask(taskId) {
+    let filteredTasks = tasks.filter((task) => {
+      return task.id !== taskId;
+    });
+
+    setTasks(filteredTasks);
+
+    saveTasksToLocalStorage(filteredTasks);
+  }
+
+  function moveTask(id, newStatus) {
+    let task = tasks.filter((task) => {
+      return task.id === id;
+    })[0];
+
+    let filteredTasks = tasks.filter((task) => {
+      return task.id !== id;
+    });
+
+    task.status = newStatus;
+
+    let newTaskList = [...filteredTasks, task];
+
+    setTasks(newTaskList);
+
+    saveTasksToLocalStorage(newTaskList);
+  }
+
+  function saveTasksToLocalStorage(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  function loadTasksFromLocalStorage() {
+    let loadedTasks = localStorage.getItem("tasks");
+
+    let tasks = JSON.parse(loadedTasks);
+
+    if (tasks) {
+      setTasks(tasks);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>ProEfficiency</h1>
+      <main>
+        <Container>
+          <Row>
+            <Col xs={4}>
+              <TaskCol
+              tasks={tasks}
+              addEmptyTask={addEmptyTask}
+              addTask={addTask}
+              deleteTask={deleteTask}
+              moveTask={moveTask}
+              status="Backlog"
+              />
+            </Col>
+            <Col xs={4}>
+              <TaskCol
+              tasks={tasks}
+              addEmptyTask={addEmptyTask}
+              addTask={addTask}
+              deleteTask={deleteTask}
+              moveTask={moveTask}
+              status="In Progress"
+              />
+            </Col>
+            <Col xs={4}>
+              <TaskCol
+              tasks={tasks}
+              addEmptyTask={addEmptyTask}
+              addTask={addTask}
+              deleteTask={deleteTask}
+              moveTask={moveTask}
+              status="Done"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </main>
+    </div>
+  );
+}
+
+export default App;
